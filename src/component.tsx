e03197b908d37becff361470852783fbbf281754
Krishan Patel <krishan711@gmail.com>
2025-03-06 09:38:13 +0000

    .

--- src/component.tsx
+++ src/component.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { getClassName, RecursivePartial } from '@kibalabs/core';
 import { Link as CoreLink, ISingleAnyChildProps, useIsCoreRoutingEnabled } from '@kibalabs/core-react';
 import { IComponentProps, themeToCss } from '@kibalabs/ui-react';
-import styled from 'styled-components';
+import { styled } from 'styled-components';
 
 import { IToastTheme } from './theme';
 
